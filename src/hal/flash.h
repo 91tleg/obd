424b48bb1b6029843503ddb4a475cf1576517f2a
91tleg <maxchou0417@gmail.com>
2026-05-12 11:35:21 -0700

    Add flash HAL for access control and wait state

---
+++ src/hal/flash.h
@@ -0,0 +1,25 @@
+/**
+ * @file    flash.h
+ * @brief   FLASH access control and wait-state configuration.
+ */
+ 
+#ifndef HAL_FLASH_H
+#define HAL_FLASH_H
+
+#include <stdint.h>
+#include "stm32h753xx.h"
+
+static inline void flash_set_latency( uint32_t latency )
+{
+    FLASH->ACR = ( FLASH->ACR & ~FLASH_ACR_LATENCY_Msk )
+               | ( latency << FLASH_ACR_LATENCY_Pos )
+               | FLASH_ACR_WRHIGHFREQ_1;  /* required for VOS1 */
+
+    while( ( FLASH->ACR & FLASH_ACR_LATENCY_Msk ) !=
+           ( latency << FLASH_ACR_LATENCY_Pos ) )
+    {
+        /* wait for latency to apply */
+    }
+}
+
+#endif /* HAL_FLASH_H */
