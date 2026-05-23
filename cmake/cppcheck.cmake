e6f1bd9b970290db195f7e6b09046f172f8f22d1
91tleg <maxchou0417@gmail.com>
2026-05-23 13:52:56 -0700

    Add cppcheck cmake

---
+++ cmake/cppcheck.cmake
@@ -0,0 +1,37 @@
+find_program(CPPCHECK cppcheck)
+
+if( CPPCHECK )
+    get_filename_component(CPPCHECK_BIN_DIR ${CPPCHECK} DIRECTORY)
+    find_file(MISRA_PY
+        NAMES misra.py
+        PATHS
+            ${CPPCHECK_BIN_DIR}/../share/cppcheck/addons
+            /usr/local/share/cppcheck/addons
+            /usr/share/cppcheck/addons
+            /opt/homebrew/share/cppcheck/addons
+        NO_DEFAULT_PATH
+    )
+
+    if( NOT MISRA_PY )
+        message(WARNING "misra.py not found")
+        set(MISRA_ADDON "")
+    else()
+        message(STATUS "Found misra.py: ${MISRA_PY}")
+        set(MISRA_ADDON "--addon=${MISRA_PY}")
+    endif()
+
+    add_custom_target(cppcheck
+        COMMAND ${CPPCHECK}
+            ${MISRA_ADDON}
+            --suppressions-list=${CMAKE_SOURCE_DIR}/cppcheck-suppressions.txt
+            --enable=all
+            --error-exitcode=1
+            --platform=arm32-wchar_t2
+            --suppress=missingIncludeSystem
+            --project=${CMAKE_BINARY_DIR}/compile_commands.json
+        COMMENT "Running cppcheck MISRA analysis"
+        VERBATIM
+    )
+else()
+    message(WARNING "cppcheck not found")
+endif()
