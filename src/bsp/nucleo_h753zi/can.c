5bc7435e917426007c0e619e437a6725a71e7174
91tleg <maxchou0417@gmail.com>
2026-05-27 22:15:00 -0700

    Add CAN BSP for nucleoh753zi

---
+++ src/bsp/nucleo_h753zi/can.c
@@ -0,0 +1,34 @@
+#include "bsp/nucleo_h753zi/can.h"
+#include "bsp/nucleo_h753zi/resources.h"
+#include "hal/gpio.h"
+#include "hal/rcc.h"
+#include "hal/can.h"
+
+result_t bsp_can_init( void )
+{
+    static can_timing_t const s_can_timing = CAN_NBTP( 10U, 1U, 29U, 10U );
+
+    static can_filter_t const s_can_filter =
+    {
+        .id_low  = 0x7E8U,   /* CAN_TP_OBD_RESP_BASE */
+        .id_high = 0x7EFU,   /* CAN_TP_OBD_RESP_MAX  */
+    };
+
+    rcc_gpio_clk_enable( BSP_CAN_TX_PORT );
+    rcc_gpio_clk_enable( BSP_CAN_RX_PORT );
+    rcc_fdcan_clk_enable();
+
+    gpio_set_mode(  BSP_CAN_TX_PORT, BSP_CAN_TX_PIN, GPIO_MODE_AF         );
+    gpio_set_af(    BSP_CAN_TX_PORT, BSP_CAN_TX_PIN, BSP_CAN_TX_AF        );
+    gpio_set_speed( BSP_CAN_TX_PORT, BSP_CAN_TX_PIN, GPIO_SPEED_HIGH      );
+    gpio_set_pull(  BSP_CAN_TX_PORT, BSP_CAN_TX_PIN, GPIO_PULL_NONE       );
+    gpio_set_otype( BSP_CAN_TX_PORT, BSP_CAN_TX_PIN, GPIO_OTYPE_PUSH_PULL );
+
+    gpio_set_mode(  BSP_CAN_RX_PORT, BSP_CAN_RX_PIN, GPIO_MODE_AF         );
+    gpio_set_af(    BSP_CAN_RX_PORT, BSP_CAN_RX_PIN, BSP_CAN_RX_AF        );
+    gpio_set_speed( BSP_CAN_RX_PORT, BSP_CAN_RX_PIN, GPIO_SPEED_HIGH      );
+    gpio_set_pull(  BSP_CAN_RX_PORT, BSP_CAN_RX_PIN, GPIO_PULL_NONE       );
+    gpio_set_otype( BSP_CAN_RX_PORT, BSP_CAN_RX_PIN, GPIO_OTYPE_PUSH_PULL );
+
+    return can_init( BSP_CAN, s_can_timing, &s_can_filter );
+}
---
+++ src/bsp/nucleo_h753zi/can.h
@@ -0,0 +1,8 @@
+#ifndef BSP_NUCLEO_H753ZI_CAN_H
+#define BSP_NUCLEO_H753ZI_CAN_H
+
+#include "lib/core/result.h"
+
+result_t bsp_can_init( void );
+
+#endif /* BSP_NUCLEO_H753ZI_CAN_H */
